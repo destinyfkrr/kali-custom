04763b1788b9845cda6a3d22012583855e6352ff
destinyfkrr <destinyfkrr@users.noreply.github.com>
2024-06-28 08:22:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,11 +11,11 @@ echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDejYsnOXr9DRpeNCfJivIZZ3/kChKxeYHQpA
 
 
 # Create necessary directories
-mkdir -p ~/vpn /opt/tools/netcat /opt/tools/chisel
+mkdir -p ~/vpn /opt/tools/netcat /opt/tools/chisel /opt/tools/peass
 
 # Download files
-wget -O /opt/tools/linpeas.sh https://github.com/peass-ng/PEASS-ng/releases/download/20240623-7d729c04/linpeas.sh
-wget -O /opt/tools/Winpeas.exe https://github.com/peass-ng/PEASS-ng/releases/download/20240623-7d729c04/winPEASany.exe
+wget -O /opt/tools/peass/linpeas.sh https://github.com/peass-ng/PEASS-ng/releases/download/20240623-7d729c04/linpeas.sh
+wget -O /opt/tools/peass/Winpeas.exe https://github.com/peass-ng/PEASS-ng/releases/download/20240623-7d729c04/winPEASany.exe
 wget -O /opt/tools/netcat/nc64.exe https://github.com/vinsworldcom/NetCat64/releases/download/1.11.6.4/nc64.exe
 wget -O /opt/tools/chisel/chisellinux.gz https://github.com/jpillora/chisel/releases/download/v1.9.1/chisel_1.9.1_linux_amd64.gz
 wget -O /opt/tools/chisel/chiselarm.gz https://github.com/jpillora/chisel/releases/download/v1.9.1/chisel_1.9.1_linux_arm64.gz
@@ -23,6 +23,7 @@ wget -O /opt/tools/chisel/chiselwin.exe.gz https://github.com/jpillora/chisel/re
 
 # Move and set permissions
 gunzip /opt/tools/chisel/chisellinux.gz
+gunzip /opt/tools/chisel/chiselwin.exe.gz
 gunzip /opt/tools/chisel/chiselarm.gz
 chmod +x /opt/tools/chisel/chisellinux /opt/tools/chisel/chiselarm
 
