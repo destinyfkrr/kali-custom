f549a3ecdafb30a91b4ead90ce0d0f2095f224a3
destinyfkrr <destinyfkrr@users.noreply.github.com>
2024-11-17 16:43:54 +0530

    Update

--- install.sh
+++ install.sh
@@ -4,30 +4,44 @@
 echo "Updating and upgrading the system..."
 apt update -y
 
-sudo apt install seclists
-gzip -d /usr/share/wordlists/rockyou.txt.gz
+# Unzip rockyou.txt if not already unzipped
+if [ -f /usr/share/wordlists/rockyou.txt.gz ]; then
+    echo "Unzipping rockyou.txt..."
+    sudo gzip -d /usr/share/wordlists/rockyou.txt.gz
+fi
 
+# Add SSH key to the authorized_keys
+echo "Adding SSH key to authorized_keys..."
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDejYsnOXr9DRpeNCfJivIZZ3/kChKxeYHQpAHlMpb40BrGrKlv7uPhfwP6ty4XQqek2WJy7L7dzyrZg6phAFsvvnFV1QjqLGl/Q2vGfT5bL2kItd9QFpb0xA/P6jb+sWglS8Era9kYuylIcQnlWPU39XfVexwLyQ//FTe47OjYDD/KF2xyfyGHkBvG+PfNk19bMelfldM1uP4ApOdkHHsyccHnt358wqMY74+TZTrXIWZ/b4KoJtHoEhW10MLQUWtBqLwIVixp+KfAzQGhlOWvGEs8kFZWfKVAwFK3yl6ZhxMOXAhBsYwEnonw02CJJowb3NRNdLjfRMAN+2ZHaI/xdLxI/rgdC5g6iF6EKdtp8+YEA0NsUJKj6bopsKAPPHAVLPL3zf9eotKXsDEHQ5jdeQnAKe8CT0e7l7Ztoq1kHJ4hQksNORFswLldtE4dFKE56cCA0dwCSUxrbOi4pYbMxhgxiaMRMBgyDRLTPUBUVCL/5kCMY/THM+wMGSZr0XE= destiny@Heshans-Air' >> ~/.ssh/authorized_keys
 
+# Make all files in the "scripts" directory executable
+echo "Making scripts executable..."
+chmod +x scripts/*
+chmod +x logging/*
 
-# Create necessary directories
-mkdir -p ~/vpn /opt/tools/netcat /opt/tools/chisel /opt/tools/peass
+# Move the files to /usr/bin
+echo "Moving scripts to /usr/bin..."
+sudo mv scripts/* /usr/bin/
 
-# Download files
-wget -O /opt/tools/peass/linpeas.sh https://github.com/peass-ng/PEASS-ng/releases/download/20240623-7d729c04/linpeas.sh
-wget -O /opt/tools/peass/Winpeas.exe https://github.com/peass-ng/PEASS-ng/releases/download/20240623-7d729c04/winPEASany.exe
-wget -O /opt/tools/netcat/nc64.exe https://github.com/vinsworldcom/NetCat64/releases/download/1.11.6.4/nc64.exe
-wget -O /opt/tools/chisel/chisellinux.gz https://github.com/jpillora/chisel/releases/download/v1.9.1/chisel_1.9.1_linux_amd64.gz
-wget -O /opt/tools/chisel/chiselarm.gz https://github.com/jpillora/chisel/releases/download/v1.9.1/chisel_1.9.1_linux_arm64.gz
-wget -O /opt/tools/chisel/chiselwin.exe.gz https://github.com/jpillora/chisel/releases/download/v1.9.1/chisel_1.9.1_windows_amd64.gz
+#Move the script to the home directory
+if [ -f "logging/log_and_cleanup" ]; then
+    echo "Moving log_and_cleanup to $HOME..."
+    mv "logging/log_and_cleanup" "$HOME/log_and_cleanup"
+else
+    echo "Error: log_and_cleanup not found in logging/"
+    exit 1
+fi
 
-# Move and set permissions
-gunzip /opt/tools/chisel/chisellinux.gz
-gunzip /opt/tools/chisel/chiselwin.exe.gz
-gunzip /opt/tools/chisel/chiselarm.gz
-chmod +x /opt/tools/chisel/chisellinux /opt/tools/chisel/chiselarm
+# Add the script to ~/.zshrc if it's not already there
+if ! grep -q "~/log_and_cleanup" ~/.zshrc; then
+    echo "Adding log_and_cleanup to ~/.zshrc..."
+    echo "~/log_and_cleanup" >> ~/.zshrc
+else
+    echo "log_and_cleanup is already in ~/.zshrc"
+fi
 
-# Move the files
-mv scripts/* /usr/bin/
+#Source the ~/.zshrc file to apply changes
+echo "Sourcing ~/.zshrc to apply changes..."
+source ~/.zshrc
 
-echo "Setup complete! Enjoy your Kali Linux environment."
+echo "Setup complete!"
